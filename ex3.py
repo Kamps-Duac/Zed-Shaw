f9ca603b7963163f38db9160df8f29b79dd65cc1
Kamps-Duac <kamps.duac@gmail.com>
2013-08-13 14:31:18 -0400

    testing yet again...

---
+++ ex3.py
@@ -0,0 +1,28 @@
+# print the string
+print "I will now count my chickens:"
+# do addition and integer division
+print "Hens", 25 + 30.0 / 6.0
+# more arithmetic
+print "Roosters", 100 - 25 * 3 % 4
+# print the string
+print "Now I will count the eggs:"
+# more arithmetic
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4.0 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+# calculates Boolean formula
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+# Python calculates the '/' operator as integer division. Using a floating point,
+# such as 20.0, will calculate the accurate division.
